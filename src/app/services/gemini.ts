ff9d999268648aa57f87921cd2b1b7b791db4b01
Umerr <112628338+Umerr-Bashir@users.noreply.github.com>
2026-01-05 00:42:03 +0500

    ssps

--- src/app/services/gemini.ts
+++ src/app/services/gemini.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class Gemini {
-  private API_KEY = "TaK8G0L1ux5XEfyo7GR6xbeWx0_pEsSfZdO2g7Rf6Zs"
+  private API_KEY = environment.POE_API_KEY
 
   constructor(
     private http: HttpClient
